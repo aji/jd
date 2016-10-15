7b01182cf444dc7ac74cac6402f7d9a7c7cfa94d
Alex Iadicicco <alex@ajitek.net>
2016-10-15 14:27:19 -0700

    Break things down into smaller functions

--- jd.py
+++ jd.py
@@ -8,32 +8,19 @@ def is_ref(node):
     return isinstance(node, type({})) and list(node.keys()) == ['$ref']
 
 class Resolver(object):
-    def __init__(self, root='.', doc=None):
-        self.root = root
-        self._doc = doc
-
-    def doc(self):
-        if self._doc is None:
-            self._doc = json.load(sys.stdin)
-        return self._doc
+    def __init__(self, path=None):
+        self.path = path
+        self._loaded_doc = None
 
     def deref(self, ref):
         path, _, frag = ref.partition('#')
 
         if path:
-            path = os.path.join(self.root, path)
-            root = os.path.dirname(path)
-            with open(path, 'r') as f:
-                doc = json.load(f)
-            return Resolver(root, doc).deref('#' + frag)
+            path = os.path.join(self._root(), path)
+            return Resolver(path).deref('#' + frag)
 
-        node = self.doc()
-        for el in [x for x in frag.split('/') if x]:
-            if is_ref(node):
-                node = self.deref(node['$ref'])
-            node = node[int(el) if el.isdigit() else el]
-
-        return self.resolve(node)
+        frag = [x for x in frag.split('/') if x]
+        return self.resolve(self._follow_frag(self._doc(), frag))
 
     def resolve(self, node=None):
         if node is None:
@@ -44,6 +31,25 @@ class Resolver(object):
             return [self.resolve(x) for x in node]
         if isinstance(node, type({})):
             return { k: self.resolve(v) for k, v in node.items() }
+        return node
+
+    def _root(self):
+        return os.path.dirname(self.path) if self.path is not None else '.'
+
+    def _doc(self):
+        if self._loaded_doc is None:
+            if self.path is not None:
+                with open(self.path, 'r') as f:
+                    self._loaded_doc = json.load(f)
+            else:
+                self._loaded_doc = json.load(sys.stdin)
+        return self._loaded_doc
+
+    def _follow_frag(self, node, frag):
+        for el in frag:
+            if is_ref(node):
+                node = self.deref(node['$ref'])
+            node = node[int(el) if el.isdigit() else el]
         return node
 
 if __name__ == '__main__':
